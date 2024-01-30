30d707da1873d1f2808dbb645b49009f832fdc8d
Yuki Liu <yuki.liu@utexas.edu>
2024-01-29 20:59:23 -0600

    remove generated unit tests

--- generated-tests/ralscha_extclassgenerator-40ad147/randoop-tests-42/randoop-tests/RegressionTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ RegressionTest0.class, RegressionTest1.class, RegressionTest2.class, RegressionTest3.class, RegressionTest4.class, RegressionTest5.class, RegressionTest6.class, RegressionTest7.class, RegressionTest8.class, RegressionTest9.class, RegressionTest10.class, RegressionTest11.class, RegressionTest12.class, RegressionTest13.class, RegressionTest14.class, RegressionTest15.class, RegressionTest16.class, RegressionTest17.class, RegressionTest18.class, RegressionTest19.class, RegressionTest20.class, RegressionTest21.class, RegressionTest22.class, RegressionTest23.class, RegressionTest24.class, RegressionTest25.class, RegressionTest26.class, RegressionTest27.class, RegressionTest28.class, RegressionTest29.class })
-public class RegressionTest {
-}
-
