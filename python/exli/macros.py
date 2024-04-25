936174479d1ee975c8fa653fb0dae95c3811134d
Yuki Liu <yuki.liu@utexas.edu>
2024-04-25 07:57:17 -0500

    update comment

--- python/exli/macros.py
+++ python/exli/macros.py
@@ -113,4 +113,5 @@ class Macros:
         "jenkinsci_email-ext-plugin",  # evosuite generated tests removed the whole directory
         "phax_ph-pdf-layout", # evosuite generated tests changed the permission of the whole directory,
         "red6_pdfcompare", # evosuite generated tests changed the permission of the whole directory,
+        "ralscha_extclassgenerator", # evosuite generated tests changed the permission of its project directory,
     ]
