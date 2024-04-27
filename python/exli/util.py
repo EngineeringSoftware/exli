45208c3b8818180f28d288860f3c08d7603c1764
Yuki Liu <yuki.liu@utexas.edu>
2024-04-27 08:34:16 -0500

    skip class

--- python/exli/util.py
+++ python/exli/util.py
@@ -796,6 +796,8 @@ class Util:
                     print("find classes...")
                     classpath_list = se.io.load(classpath_file_path, se.io.Fmt.txtList)
                     for classpath in tqdm(classpath_list):
+                        if classpath in ["de.redsix.pdfcompare.PdfComparator$InputStreamSupplier"]:
+                            continue
                         command = f"java -jar {Macros.evosuite_jar} -DCP_file_path {dep_file_path} -class {classpath} -seed {seed} -Dsearch_budget={time_limit} -Duse_separate_classloader=false -Dminimize=false -Dassertion_strategy=all -Dfilter_assertions=true -Dvirtual_fs=false -Dvirtual_net=false -Dsandbox_mode=OFF -Dfilter_sandbox_tests=true -Dmax_loop_iterations=-1 &> {log_path}"
                         print(command)
                         try:
