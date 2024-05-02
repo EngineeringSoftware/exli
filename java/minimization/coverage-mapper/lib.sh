7e5baaf3cc809573da4bf709299720b427f464c1
Yuki Liu <yuki.liu@utexas.edu>
2024-05-02 14:31:14 -0500

    update target dir

--- java/minimization/coverage-mapper/lib.sh
+++ java/minimization/coverage-mapper/lib.sh
@@ -1,8 +1,8 @@
 # downlaod asm-9.2.jar, asm-commons-9.2.jar, asm-tree-9.2.jar, jacoco.core-0.8.7.jar, jacoco.report-0.8.7.jar, jacococli.jar, json.jar
 mkdir lib
-wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.core/0.8.7/org.jacoco.core-0.8.7.jar lib/jacoco.core-0.8.7.jar
-wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.report/0.8.7/org.jacoco.report-0.8.7.jar lib/jacoco.report-0.8.7.jar
-wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacococli/0.8.7/org.jacococli-0.8.7.jar lib/jacococli.jar
-wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm/9.2/asm-9.2.jar lib/asm-9.2.jar
-wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/9.2/asm-commons-9.2.jar lib/asm-commons-9.2.jar
-wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-tree/9.2/asm-tree-9.2.jar lib/asm-tree-9.2.jar
+wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.core/0.8.7/org.jacoco.core-0.8.7.jar -p lib/jacoco.core-0.8.7.jar
+wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.report/0.8.7/org.jacoco.report-0.8.7.jar -p lib/jacoco.report-0.8.7.jar
+wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacococli/0.8.7/org.jacococli-0.8.7.jar -p lib/jacococli.jar
+wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm/9.2/asm-9.2.jar -p lib/asm-9.2.jar
+wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/9.2/asm-commons-9.2.jar -p lib/asm-commons-9.2.jar
+wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-tree/9.2/asm-tree-9.2.jar -p lib/asm-tree-9.2.jar
