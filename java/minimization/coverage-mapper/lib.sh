1c7a76424289f8b82eafdfc93a7c9ff95b3465f5
Yuki Liu <yuki.liu@utexas.edu>
2024-05-02 14:59:25 -0500

    fix script output

--- java/minimization/coverage-mapper/lib.sh
+++ java/minimization/coverage-mapper/lib.sh
@@ -1,9 +1,9 @@
 # downlaod asm-9.2.jar, asm-commons-9.2.jar, asm-tree-9.2.jar, jacoco.core-0.8.7.jar, jacoco.report-0.8.7.jar, jacococli.jar, json.jar
 mkdir lib
-wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.core/0.8.7/org.jacoco.core-0.8.7.jar -P lib/jacoco.core-0.8.7.jar
-wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.report/0.8.7/org.jacoco.report-0.8.7.jar -P lib/jacoco.report-0.8.7.jar
-wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.cli/0.8.7/org.jacoco.cli-0.8.7.jar -P lib/jacococli.jar
-wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm/9.2/asm-9.2.jar -P lib/asm-9.2.jar
-wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/9.2/asm-commons-9.2.jar -P lib/asm-commons-9.2.jar
-wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-tree/9.2/asm-tree-9.2.jar -P lib/asm-tree-9.2.jar
-wget -q https://repo1.maven.org/maven2/org/json/json/20210307/json-20210307.jar -P lib/json.jar
+wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.core/0.8.7/org.jacoco.core-0.8.7.jar -O lib/jacoco.core-0.8.7.jar
+wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.report/0.8.7/org.jacoco.report-0.8.7.jar -O lib/jacoco.report-0.8.7.jar
+wget -q https://repo1.maven.org/maven2/org/jacoco/org.jacoco.cli/0.8.7/org.jacoco.cli-0.8.7.jar -O lib/jacococli.jar
+wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm/9.2/asm-9.2.jar -O lib/asm-9.2.jar
+wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/9.2/asm-commons-9.2.jar -O lib/asm-commons-9.2.jar
+wget -q https://repo1.maven.org/maven2/org/ow2/asm/asm-tree/9.2/asm-tree-9.2.jar -O lib/asm-tree-9.2.jar
+wget -q https://repo1.maven.org/maven2/org/json/json/20210307/json-20210307.jar -O lib/json.jar
